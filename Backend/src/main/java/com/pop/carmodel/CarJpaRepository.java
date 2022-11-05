2f46cc9fb9806ce42d2c1433199aceed77160bfb
KacperHolubowicz <kac.perhol2000@gmail.com>
2022-11-05 14:34:50 +0100

    Somehow implemented car model layer

--- Backend/src/main/java/com/pop/carmodel/CarJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pop.carmodel;
-
-import com.pop.carmodel.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarJpaRepository extends JpaRepository<Car, Long> {
-}
-
--- Backend/src/main/java/com/pop/carmodel/CarModelRestController.java
+++ Backend/src/main/java/com/pop/carmodel/CarModelRestController.java
@@ -1,13 +1,37 @@
 package com.pop.carmodel;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/car")
+@AllArgsConstructor
 public class CarModelRestController {
+    private final ICarModel carModel;
 
-    @GetMapping("/")
-    public String index() {
-        return "<h1>teścik agreścik</h1>";
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<CarDataDTO>> getListOfCars(@PathVariable long userId) {
+        try {
+            List<CarDataDTO> cars = carModel.getListCar(userId);
+            return ResponseEntity.ok(cars);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Boolean> saveCar(@RequestBody CarDataDTO carData) {
+        try {
+            if (carModel.saveCar(carData)) {
+                return new ResponseEntity<>(true, HttpStatus.CREATED);
+            }
+            return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
