abe0ae36683dd450eb40d8b9355a634a50eee325
KacperHolubowicz <kac.perhol2000@gmail.com>
2022-12-30 14:35:45 +0100

    The most advance payment mock implemented, diagram changes

--- Backend/src/main/java/com/pop/feesmodel/service/IPaymentService.java
+++ Backend/src/main/java/com/pop/feesmodel/service/IPaymentService.java
@@ -1,5 +1,13 @@
 package com.pop.feesmodel.service;
 
 public interface IPaymentService {
+    /*
+        Szansa na udaną płatnośc zależna od wartości paymentType:
+        1 - 100% szans na udaną płatność
+        2 - 75% szans
+        3 - 50% szans
+        4 - 25% szans
+        Pozostałe wartości - 0% szans (zawsze nieudana)
+     */
     boolean launchPayment(long driverId, int paymentType);
 }
--- Backend/src/main/java/com/pop/feesmodel/service/PaymentServiceProxy.java
+++ Backend/src/main/java/com/pop/feesmodel/service/PaymentServiceProxy.java
@@ -1,9 +1,11 @@
 package com.pop.feesmodel.service;
 
+import com.pop.payment.PaymentMock;
+
 public class PaymentServiceProxy implements IPaymentService {
 
     @Override
     public boolean launchPayment(long driverId, int paymentType) {
-        return false;
+        return PaymentMock.createPayment(paymentType);
     }
 }
