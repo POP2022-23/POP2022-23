d243f264b66cb2bce02f313399087ae10f3dc837
Kasprzak-Arkadiusz <Akasprzak016@gmail.com>
2023-01-07 20:59:01 +0100

    Adapt to changed DTO

--- Frontend/road-management/src/views/PaidFees/FeeDetailsTable.tsx
+++ Frontend/road-management/src/views/PaidFees/FeeDetailsTable.tsx
@@ -1,24 +1,23 @@
 ﻿import React from 'react'
-import {FeesDTO, VehicleType} from "../../interfaces/fees/feesinterfaces";
+import {FeesDTO} from "../../interfaces/fees/feesinterfaces";
 import FeeDetailsTableRow from "./FeeDetailsTableRow";
 import {VehicleTypeStrings} from "../../utils/enumExtensions";
 
 interface IFeeDetailsTable {
-  feeDetails: FeesDTO | undefined;
+    feeDetails: FeesDTO | undefined;
 }
 
 function FeeDetailsTable({feeDetails}: IFeeDetailsTable) {
-  return feeDetails === undefined ? <p>Brak danych dla tej opłaty</p> :
-    (
-      <table className="paid-fees-details-table">
-        <tbody>
-        <FeeDetailsTableRow label="Kwota" value={`${feeDetails.amount} zł`}/>
-        <FeeDetailsTableRow label="Data" value={feeDetails.date.toISOString().split('T')[0]}/>
-        <FeeDetailsTableRow label="Taryfa" value={feeDetails.tariff.name}/>
-        <FeeDetailsTableRow label="Typ pojazdu" value={VehicleTypeStrings[VehicleType[feeDetails.vehicleType]]}/>
-        </tbody>
-      </table>
-    )
+    return feeDetails === undefined ? <p>Brak danych dla tej opłaty</p> :
+        (
+            <table className="paid-fees-details-table">
+                <tbody>
+                <FeeDetailsTableRow label="Kwota" value={`${feeDetails.amount} zł`}/>
+                <FeeDetailsTableRow label="Data wystawienia" value={feeDetails.issueDate.split('T')[0]}/>
+                <FeeDetailsTableRow label="Data opłacenia" value={feeDetails.expirationDate.split('T')[0]}/>
+                </tbody>
+            </table>
+        )
 }
 
 export default FeeDetailsTable;
--- Frontend/road-management/src/views/PaidFees/FeeListTableRow.tsx
+++ Frontend/road-management/src/views/PaidFees/FeeListTableRow.tsx
@@ -4,24 +4,24 @@ import {VehicleTypeStrings} from "../../utils/enumExtensions";
 import {Button} from "react-bootstrap";
 
 interface IProps {
-  feeId: number;
-  date: Date;
-  tariffName: string;
-  vehicleType: VehicleType;
-  onViewDetailsClicked: (feeId: number) => void;
+    feeId: number;
+    issueDate: string;
+    tariffName: string;
+    vehicleType: VehicleType;
+    onViewDetailsClicked: (feeId: number) => void;
 }
 
-function FeeListTableRow({feeId, date, tariffName, vehicleType, onViewDetailsClicked}: IProps) {
-  return (
-    <tr>
-      <td>{date.toISOString().split('T')[0]}</td>
-      <td>{tariffName}</td>
-      <td>{VehicleTypeStrings[VehicleType[vehicleType]]}</td>
-      <td>
-        <Button onClick={() => onViewDetailsClicked(feeId)}>Zobacz szczegóły</Button>
-      </td>
-    </tr>
-  )
+function FeeListTableRow({feeId, issueDate, tariffName, vehicleType, onViewDetailsClicked}: IProps) {
+    return (
+        <tr>
+            <td>{issueDate.split('T')[0]}</td>
+            <td>{tariffName}</td>
+            <td>{VehicleTypeStrings[vehicleType]}</td>
+            <td>
+                <Button onClick={() => onViewDetailsClicked(feeId)}>Zobacz szczegóły</Button>
+            </td>
+        </tr>
+    )
 }
 
 export default FeeListTableRow
